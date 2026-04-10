d12d091c14d1997b0e445190331818e9dd48184d
Dev <devnmdv@test.com>
2026-04-10 17:56:47 +0530

    .env issue resolved

--- Frontend/src/features/home/service/song.api.js
+++ Frontend/src/features/home/service/song.api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import dotenv from 'dotenv'
-dotenv.config()
-
+const apiUrl = import.meta.env.VITE_BACKEND_URL
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: apiUrl,
     withCredentials: true
 })
 
